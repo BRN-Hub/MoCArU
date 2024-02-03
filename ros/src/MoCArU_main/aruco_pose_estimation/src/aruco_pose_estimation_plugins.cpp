0e15e56869d4688dbd23af39d609c5f53f3a9f39
rawias <66157015+rawias@users.noreply.github.com>
2024-02-03 16:48:52 +0700

    Add the experiment gif to the images branch

--- ros/src/MoCArU_main/aruco_pose_estimation/src/aruco_pose_estimation_plugins.cpp
+++
@@ -1,5 +0,0 @@
-#include <aruco_pose_estimation/relative_pose_estimator.hpp>
-#include <nodelet/nodelet.h>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(aruco_pose_estimation::RelativePoseEstimator, nodelet::Nodelet);
--- ros/src/MoCArU_main/aruco_recognition/src/aruco_recognition_plugins.cpp
+++
@@ -1,5 +0,0 @@
-#include <aruco_recognition/aruco_reader.hpp>
-#include <nodelet/nodelet.h>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(aruco_recognition::ArUcoReader, nodelet::Nodelet);
